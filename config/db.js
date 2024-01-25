f1896d5d36b262e8d52da61466cf8288696e5cbf
Kian Naquines <kjgnaquines@gmail.com>
2024-01-25 15:36:18 +0800

    commit msg

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export function dbconnection() {
-    mongoose.connect('mongodb+srv://kjgnaquines:password0000@cluster0.rkiolj8.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    mongoose.connect('mongodb+srv://kjgnaquines:3LPAnunIakXjffLm@clusterpermit.ftpcqy1.mongodb.net/?retryWrites=true&w=majority').then(() => {
         console.log('MongoDB Connection established successfully')   
     }).catch((err) => {
         console.log(err)
