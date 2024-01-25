409a6bb5b16774ad75c92434c579b9d22799bd22
Kian Naquines <kjgnaquines@gmail.com>
2024-01-25 15:12:08 +0800

    edit db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export function dbconnection() {
-    mongoose.connect('mongodb://127.0.0.1:27017/permit_management_systemDB').then(() => {
+    mongoose.connect('mongodb+srv://kjgnaquines:password0000@cluster0.rkiolj8.mongodb.net/?retryWrites=true&w=majority').then(() => {
         console.log('MongoDB Connection established successfully')   
     }).catch((err) => {
         console.log(err)
